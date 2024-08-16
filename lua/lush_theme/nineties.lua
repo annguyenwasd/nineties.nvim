9e51795feab756bbf465352a3e17a259d2683206
An Nguyen <an.nguyenwasd@gmail.com>
2024-08-16 18:56:53 +0700

    Sync Aug 16 2024

--- lua/lush_theme/nineties.lua
+++ lua/lush_theme/nineties.lua
@@ -101,7 +101,7 @@ local theme = lush(function(injected_functions)
 		CursorLineNr({ fg = Normal.fg, bg = Normal.bg, gui = "bold" }), -- Like LineNr when 'cursorline' or 'relativenumber' is set for the cursor line.
 		-- CursorLineFold { }, -- Like FoldColumn when 'cursorline' is set for the cursor line
 		-- CursorLineSign { }, -- Like SignColumn when 'cursorline' is set for the cursor line
-		MatchParen({ fg = black, bg = green }), -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
+		-- MatchParen({ fg = black, bg = green }), -- Character under the cursor or just before it, if it is a paired bracket, and its match. |pi_paren.txt|
 		-- ModeMsg        { }, -- 'showmode' message (e.g., "-- INSERT -- ")
 		-- MsgArea        { }, -- Area for messages and cmdline
 		-- MsgSeparator   { }, -- Separator for scrolled messages, `msgsep` flag of 'display'
